53d6d1901bfa5d356c95bc3212d24559b0242a3c
Nestoras Sdoukos <sdoukos@csd.uco.gr>
2019-08-09 18:16:50 +0300

    Change Statistics block to be more useful.
    Noise is generated by the block after the signal power is calculated.
    User can export the detection ratio results to a csv file. The block
    now is useful only for simulated environment.

--- python/Statistics.py
+++ python/Statistics.py
@@ -21,105 +21,130 @@
 
 import numpy
 import pmt
+import math
+import os
+import pandas
+import matplotlib
+import matplotlib.pyplot as plt
+
 from gnuradio import gr
+from gnuradio import analog
 
 class Statistics(gr.sync_block):
     """
     docstring for block Statistics
     """
-    def __init__(self, samp_rate, signal_on, modulation, nf_estimation_time):
+    def __init__(self, samp_rate, signal_on, signal_estimation_time, iterations,\
+                 snr_list, export_csv, csv_file_path, export_plots, plot_file_path,\
+                 modulation, false_alarm):
         gr.sync_block.__init__(self,
             name="Statistics",
             in_sig=[numpy.complex64],
             out_sig=[numpy.complex64])
+        # Initialize private members
         self.samp_rate = samp_rate
         self.signal_on = signal_on
+        self.signal_estimation_time = signal_estimation_time
+        self.iterations = iterations
+        self.snr_list = snr_list
+        self.export_csv = export_csv
+        self.csv_file_path = csv_file_path
         self.modulation = modulation
-        self.nf_estimation_time = nf_estimation_time
-        self.nf_estimation_samples = nf_estimation_time * samp_rate
-        self.noise_samples = 0
-        self.signal_samples = 0
-        self.noise_power = 0
-        self.noise_power_list = []
-        self.signal_noise_power = 0
+        self.false_alarm = false_alarm
+        self.signal_estimation_samples = self.signal_estimation_time * samp_rate
+        self.snr_index = 0
+        self.signal_samples = 0.0
+        self.signal_power = 0.0
         self.signal_power_list = []
-        self.snr = 0
-        self.detection_ratio = 0
+        self.noise_power = 0.0
+        self.signal_noise_power = 0.0
+        self.signal_power_list = []
+        #self.snr = 0.0
+        self.detection_ratio = 0.0
+        self.detection_ratio_list = []
         self.false_alarm_ratio = 0.0
         self.detected = 0.0
-        self.detected_signal = 0
-        self.detected_no_signal = 0
-        self.flag2 = 0
+        self.detected_signal = 0.0
+        self.detected_no_signal = 0.0
+        # Initialize AWGN
+        self.noise = analog.fastnoise_source_c(analog.GR_GAUSSIAN, 0.0, 0, 8192)
+        # Initialize message port
         self.message_port_register_in(pmt.intern("data_in"))
         self.set_msg_handler(pmt.intern("data_in"), self.handle_msg)
-
-    def __del__(self):
-        print "EKALESTHKA"
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         out = output_items[0]
         # <+signal processing here+>
-        if not self.signal_on and self.noise_samples < self.nf_estimation_samples:
-            self.noise_samples += len(input_items[0])
-            self.noise_power_estimation(in0, len(input_items[0]))
-        elif self.signal_on and self.signal_samples < self.nf_estimation_samples:
-            self.signal_samples += len(input_items[0])
-            self.power_estimation(in0, len(input_items[0]))
-        
-        '''
-        if self.signal_on and self.detected > 0:
-            print "Detection Ratio: ", self.detected_signal/self.detected
-        elif self.signal_on and self.detected == 0:
-            print "Detection Ratio: ", 0
-        '''
-        '''
-        if not self.signal_on and self.detected > 0:
-            print "False Alarm: ", self.detected_no_signal/self.detected
-        elif not self.signal_on and self.detected == 0:
-            print "False Alarm: ", 0
-        '''
-        if self.detected >= 1000:
+        out[:] = in0
+
+        if self.signal_samples < self.signal_estimation_samples:
+            self.signal_samples += len(in0)
+            self.signal_power_estimation(in0, len(in0))
+        else:
+            if self.noise_power == 0.0:
+                self.noise_power = self.calculate_noise_power(self.signal_power, self.snr_list[self.snr_index])
+                self.noise.set_amplitude(self.noise_power)
+
+            for i in range(0, len(out)):
+                out[i] = in0[i] + self.noise.sample()    
+                    
+        if self.detected >= self.iterations:
+            print "---------------------------------------------------------"
+            print "SNR: ", self.snr_list[self.snr_index]
             print "Detection Ratio: ", self.detected_signal/self.detected
             print "Algorithm responses: ", self.detected
             print "Detected: ", self.detected_signal
+            self.detection_ratio = self.detected_signal/self.detected
+            self.detection_ratio_list.append(self.detection_ratio)
+            self.noise_power = 0.0
+            self.snr_index += 1
+            self.detected = 0.0
+            self.detected_signal = 0.0
+            self.detected_no_signal = 0
+
+        if len(self.snr_list) == self.snr_index:
+            if (self.export_csv):
+                self.export_csv_detection_ratio(self.snr_list, self.detection_ratio_list, self.csv_file_path)
             return -1
         
-        out[:] = in0
         return len(output_items[0])
 
-    def noise_power_estimation(self, samples, input_items):
-        estimated_power = numpy.sum(numpy.absolute(samples) ** 2) / input_items
-        self.noise_power_list.append(estimated_power)
-        if self.signal_on or self.noise_samples >= self.nf_estimation_samples:
-            self.noise_power = sum(self.noise_power_list) / len(self.noise_power_list)
-            print "noise power: ", self.noise_power
-
-    def power_estimation(self, samples, input_items):
+    def signal_power_estimation(self, samples, input_items):
         estimated_power = numpy.sum(numpy.absolute(samples) ** 2) / input_items
         self.signal_power_list.append(estimated_power)
-        if self.signal_samples >= self.nf_estimation_samples:
-            self.signal_noise_power = sum(self.signal_power_list) / len(self.signal_power_list)
-            print "signal - noise: ", (self.signal_noise_power - self.noise_power)
-            self.snr = 10 * numpy.log10((self.signal_noise_power - self.noise_power) /
-                                      self.noise_power)
-            print "SNR: ", self.snr
+        if self.signal_samples >= self.signal_estimation_samples:
+            self.signal_power = sum(self.signal_power_list) / len(self.signal_power_list)
 
-        return estimated_power 
-
+    def calculate_noise_power(self, signal_power, snr):
+        noise_power = signal_power * (10.0**(-snr/10.0))
+        print noise_power
+        return math.sqrt(noise_power)
+    
     def stats(self, msg):
         if pmt.to_python(msg) or self.signal_on:
             self.detected += 1
         if pmt.to_python(msg) and self.signal_on:
             self.detected_signal += 1
         if pmt.to_python(msg) and not self.signal_on:
-            #print msg
             self.detected_no_signal += 1
 
     def handle_msg(self, msg):
-        self.stats(msg)
-        pass
+        if self.signal_samples >= self.signal_estimation_samples:
+            self.stats(msg)
 
     def set_signal(self, on):
         self.signal_on = on
-        print self.signal_on
+
+    def plot(self, snr_list, detection_ratio, path):
+        pass
+
+    def export_csv_detection_ratio(self, snr_list, detection_ratio_list, path):
+        if (os.path.isdir(path)):
+            path += '/' + 'Detection_ratio_' + str(self.modulation) + '_' + str(self.samp_rate) + 'sps_' +\
+                    str(self.false_alarm) + 'prob_fa' + '.csv'
+        df1 = pandas.DataFrame(snr_list, columns = ['SNR'])
+        df2 = pandas.DataFrame(detection_ratio_list, columns = ['Detection_Ratio'])
+
+        data = pandas.concat([df1, df2], axis = 1)
+        data.to_csv(path, index = False)
